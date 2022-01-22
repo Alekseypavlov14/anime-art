fcacf5cb1c263e21c868279b9f9af5d8ec8e8652
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-01-22 21:35:04 +0200

    fix bag with order of posts

--- src/components/ViewBox/Views/IdeasPage.js
+++ src/components/ViewBox/Views/IdeasPage.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react"
 import Article from "./Article/Article"
 import styles from './IdeasPage.module.css'
-import posts from './../../../posts.json'
+import postsData from './../../../posts.json'
 
 const IdeasPage = () => {
+    const posts = []
+
+    for (key, value of postsData) {
+        posts[key] = value
+    }
+
     return(
         <div className={styles.IdeasPage}>
             {posts.reverse().map(post => (
